015aa61884790700efad5d224b9781c39192ac75
unknown <nourhene803@gmail.com>
2024-10-25 14:37:32 +0100

    first commit

--- python/Week_1/Day_4/Practice/assignment_checkerboard/server.py
+++ python/Week_1/Day_4/Practice/assignment_checkerboard/server.py
@@ -1,10 +1,14 @@
 from flask import Flask,render_template
 app=Flask(__name__)  
-
 @app.route("/") 
 def index(): 
     return render_template("index.html")
-@app.route("/number")
+@app.route("/<int:x>")
+def index2(x):
+    return render_template("index3.html",x=x)
+@app.route("/<int:x>/<int:y>")
+def index3(x,y):
+    return render_template("index2.html",x=x,y=y)
 
 if __name__=="__main__": # Ensure this file is being run directly
     app.run(debug=True) #  
